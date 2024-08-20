4cf4f1df42b394f128901bf917a5da895a73dc1f
tigaron <suputra@mail.com>
2024-08-20 19:15:09 +0700

    fix: pascals-triangle

--- 118-pascals-triangle/main.go
+++ 118-pascals-triangle/main.go
@@ -2,20 +2,13 @@ package main
 
 func generate(numRows int) [][]int {
 	results := make([][]int, numRows)
-	for i := range numRows {
+
+	for i := 0; i < numRows; i++ {
 		results[i] = make([]int, i+1)
-		results[i][0] = 1
-	}
+		results[i][0], results[i][i] = 1, 1
 
-	if numRows == 1 {
-		return results
-	}
-
-	for k := 1; k < numRows; k++ {
-		for i := 1; i < len(results[k]); i++ {
-			for j := i; j > 0; j-- {
-				results[k][j] = results[k][j] + results[k][j-1]
-			}
+		for j := 1; j < i; j++ {
+			results[i][j] = results[i-1][j-1] + results[i-1][j]
 		}
 	}
 
