fc2d3d948e3bbd8ce4ce78dc464bef2ceb45ff1c
tigaron <suputra@mail.com>
2024-08-20 19:23:55 +0700

    fix: find-k-th-smallest-pair-distance go

--- 719-find-k-th-smallest-pair-distance/main.go
+++ 719-find-k-th-smallest-pair-distance/main.go
@@ -5,29 +5,27 @@ import "sort"
 func smallestDistancePair(nums []int, k int) int {
 	sort.Ints(nums)
 
-	left, right := 0, nums[len(nums)-1]-nums[0]
+	low, high := 0, nums[len(nums)-1]-nums[0]
 
-	for left < right {
-		mid := left + (right-left)/2
+	for low < high {
+		mid := low + (high-low)/2
 
-		if countPairsWithDistanceLessThanOrEqual(nums, mid) < k {
-			left = mid + 1
+		count, start := 0, 0
+
+		for end := 0; end < len(nums); end++ {
+			for nums[end]-nums[start] > mid {
+				start++
+			}
+			count += end - start
+		}
+
+		if count >= k {
+			high = mid
 		} else {
-			right = mid
+			low = mid + 1
 		}
+
 	}
 
-	return left
-}
-
-func countPairsWithDistanceLessThanOrEqual(arr []int, mid int) int {
-	count := 0
-	for i := 0; i < len(arr); i++ {
-		j := i + 1
-		for j < len(arr) && arr[j]-arr[i] <= mid {
-			j++
-		}
-		count += j - i - 1
-	}
-	return count
+	return low
 }
