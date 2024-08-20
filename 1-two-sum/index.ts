ffe436902263103592add64268cd2a35b229811c
tigaron <suputra@mail.com>
2024-08-20 19:02:08 +0700

    fix: two-sum ts

--- 1-two-sum/index.ts
+++ 1-two-sum/index.ts
@@ -1,11 +1,11 @@
 function twoSum(nums: number[], target: number): number[] {
+  const numMap = new Map<number, number>();
   for (let i = 0; i < nums.length; i++) {
     const diff = target - nums[i];
-    const diffIndex = nums.indexOf(diff);
-
-    if (diffIndex !== -1 && diffIndex !== i) {
-      return [i, diffIndex];
+    if (numMap.has(diff)) {
+      return [numMap.get(diff)!, i];
     }
+    numMap.set(nums[i], i);
   }
 
   return [];
