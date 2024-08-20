3571b731bf720117f8c9044fdd3402e45eddcb8a
tigaron <suputra@mail.com>
2024-08-20 19:18:58 +0700

    fix: best-time-to-buy-and-sell-stock go

--- 121-best-time-to-buy-and-sell-stock/main.go
+++ 121-best-time-to-buy-and-sell-stock/main.go
@@ -1,16 +1,17 @@
 package main
 
 func maxProfit(prices []int) int {
-	if len(prices) < 2 {
-		return 0
-	}
-
 	maxProfit := 0
-	currentProfit := 0
+	left := 0
+	right := 1
 
-	for i := 1; i < len(prices); i++ {
-		currentProfit = max(0, currentProfit+prices[i]-prices[i-1])
-		maxProfit = max(maxProfit, currentProfit)
+	for right < len(prices) {
+		if prices[left] > prices[right] {
+			left = right
+		} else {
+			maxProfit = max(maxProfit, prices[right]-prices[left])
+		}
+		right++
 	}
 
 	return maxProfit
