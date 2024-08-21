d69b9d99c42fdde03287c91215c25f3361b94f07
tigaron <suputra@mail.com>
2024-08-21 18:40:12 +0700

    feat: remove-element go

---
+++ 27-remove-element/main.go
@@ -0,0 +1,14 @@
+package main
+
+func removeElement(nums []int, val int) int {
+	ans := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[ans] = nums[i]
+			ans++
+		}
+	}
+
+	return ans
+}
